59d79b17330d7d972929711ce130e569f09c7bc1
mateo <atehortuamateo123@gmail.com>
2023-08-17 09:05:20 -0500

    routes task

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 // se importan las librerias y archivos necesarios
 import express from 'express';
 import morgan from 'morgan';
-import authRoutes from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser';
+
+//rutas
+import authRoutes from './routes/auth.routes.js';
+import taskRoutes from './routes/tasks.routes.js';
+ 
 
 const app = express();
 
 // middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(cookieParser());
 // routes
 app.use('/api' , authRoutes);
+app.use('/api' , taskRoutes);
 
 // exporta el modulo de express para usarlo en index.js
 export default app;
--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () =>{
    try{    
       await mongoose.connect('mongodb+srv://admin:moebiuz123@moebiuz.zj2ifg6.mongodb.net/?retryWrites=true&w=majority')
-      console.log("funciona :D");
+      console.log("db it working");
 
    }
    catch(error){ 
