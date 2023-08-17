8bdd69140b93cdd117dca65c96862a481406b176
pabrax <pabloeg0420@outlook.com>
2023-08-16 22:45:46 -0500

    fix: task routes

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,12 +7,15 @@ import {
     updateTask, 
     deleteTask 
 } from "../controllers/tasks.controller.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
+
 
 const router = Router();
 
 router.get('/tasks', authRequired, getTasks);
 router.get('/tasks/:id', authRequired, getTask);
-router.post('/tasks', authRequired, createTask);
+router.post('/tasks', authRequired, validateSchema(createTask), createTask);
 router.delete('/tasks/:id', authRequired, deleteTask);
 router.put('/tasks/:id', authRequired, updateTask);
 
