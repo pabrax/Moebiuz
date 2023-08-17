db748bceae04249cb325f703e9144e4ac844af1e
pabrax <pabloeg0420@outlook.com>
2023-08-16 22:41:54 -0500

    feature: data Validation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import {register, login} from '../controllers/auth.controller.js';
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateSchema(registerSchema), register);
+router.post('/login', validateSchema(loginSchema), login);
 
 export default router;
